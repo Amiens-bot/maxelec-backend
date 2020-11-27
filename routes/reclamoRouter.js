7c1389bf39b099507586ed4d859c9239c40d7809
maurirlz <mauribenitez95@gmail.com>
2020-11-27 02:30:28 -0300

    add comment to hint about endpoint usage

--- routes/reclamoRouter.js
+++ routes/reclamoRouter.js
@@ -5,11 +5,12 @@ const { db } = require('../model/db');
 // example endpoint: /api/reclamos/clienteoverview?numero_serie=123&numero_factura=321
 router.route('/clienteoverview').get(reclamoController.getOverview(db));
 
-// example endpoint: /api/reclamos/pendientes/1231233
+// example endpoint: /api/reclamos/pendientes/{numerodecuit}
 router
   .route('/pendientes/:cuit')
   .get(reclamoController.getReclamosPendientes(db));
 
+// example endpoint: /api/reclamos/engestion/{numerodecuit}
 router
   .route('/engestion/:cuit')
   .get(reclamoController.getReclamosEnGestion(db));
