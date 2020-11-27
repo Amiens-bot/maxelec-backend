88e4603727b540c358c54347e999920676589002
maurirlz <mauribenitez95@gmail.com>
2020-11-27 00:55:58 -0300

    Fix on reclamos

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
-const ciudadesRouter = require("./routes/ciudadesRouter");
-const tecnicosExternosRouter = require("./routes/tecnicosExternosRouter");
+const ciudadesRouter = require('./routes/ciudadesRouter');
+const tecnicosExternosRouter = require('./routes/tecnicosExternosRouter');
+const reclamoRouter = require('./routes/reclamoRouter');
 
 const app = express();
 
@@ -13,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 // Routers
-app.use("/api/ciudades", ciudadesRouter);
-app.use("/api/tecnicosexternos", tecnicosExternosRouter);
+app.use('/api/ciudades', ciudadesRouter);
+app.use('/api/tecnicosexternos', tecnicosExternosRouter);
+app.use('/api/reclamo', reclamoRouter);
 
 module.exports = app;
